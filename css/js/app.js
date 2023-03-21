7b7f4293a865d3f9e8ef6a801e8f8d7f2a4929b0
hamid <sik6883@gmail.com>
2023-03-21 14:53:33 +0600

    secnd commit

--- css/js/app.js
+++ css/js/app.js
@@ -5,24 +5,45 @@ const lodedatatoApi = ()=>{
     .then(data => prosesdata(data))
 }
 const prosesdata = (countrys)=>{
-console.log(countrys);
+// console.log(countrys);
 const allcountrisHtml = countrys.map(country=>sodataUI(country))
-console.log(allcountrisHtml);
+// console.log(allcountrisHtml);
 const countris = document.getElementById('countris');
 countris.innerHTML = allcountrisHtml.join(' ');
 }
 
-const sodataUI = (country)=>{ 
+const sodataUI = (data)=>{ 
+    // weay 1
+    
+// return`
+// <div class="countri">
+// <h2>
+// ${data.name.common}
+// </h2>
+
+// <h3>${data.capital}</h3>
+// <img src="${data.flags.png}" >
+
+// </div>
+// `
+// way 2 use distracring
+console.log(data);
+const {name,flags,capital,area,region}= data;
+   
 return`
 <div class="countri">
 <h2>
-${country.name.common}
+${name.common}
 </h2>
 
-<h3>${country.capital}</h3>
-<img src="${country.flags.png}" >
+<h3>Capital:${capital}</h3>
+<img src="${flags.png}" >
+<p>Area:${area}</p>
+<p>Region:${region}</p>
 
 </div>
 `
 }
+
+
 lodedatatoApi()
